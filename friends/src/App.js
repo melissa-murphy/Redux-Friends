d56234e11edb02d41bcfbbb0f2cdef0dec9c0ee1
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-16 18:39:29 -0700

    default App, containers created

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,1 +1,11 @@
 import React, { Component } from "react";
+
+import { FriendListContainer } from './store/containers/FriendListContainer';
+
+class App extends Component {
+    render() {
+        return <FriendListContainer />>
+    }
+}
+
+export default App;
