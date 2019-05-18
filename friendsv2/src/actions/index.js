ea03ca8ae3b6f415da4a53fd9a8b1a1d45c9a7db
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-18 15:13:02 -0700

    fetch friends action

--- friendsv2/src/actions/index.js
+++ friendsv2/src/actions/index.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
 import { axiosAuth } from '../axiosAuth';
+import { dispatch } from '../../../../../../../AppData/Local/Microsoft/TypeScript/3.4.5/node_modules/rxjs/internal/observable/pairs';
 
 // Fetch + Initial Render
 export const FETCHING_FRIENDS = 'FETCHING_FRIENDS';
 export const FETCH_FRIENDS_SUCESS = 'FETCH_FRIENDS_SUCESS';
 export const FETCH_FRIENDS_FAULURE = 'FETCH_FRIENDS_FAULURE';
+export const fetchFriends = () => {
+  dispatch({ type: FETCHING_FRIENDS });
+  console.log(`---------------fetching friends`);
+  axiosAuth()
+    .get(`http://localhost:5000/api/friends/`)
+    .then(res => {
+      console.log(res.data);
+      dispatch({ FETCH_FRIENDS_SUCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_FRIENDS_FAULURE });
+    });
+};
 
 // Add Friends
 export const ADDING_FRIENDS = 'ADDING_FRIENDS';
