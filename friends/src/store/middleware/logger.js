b0977766f4f9e3dce476ecd2888768ded5a5fa8d
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-18 13:23:11 -0700

    look at the overcomplicated mess! I know better

--- friends/src/store/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-export const logger = store => next => action => {
-    console.group(action.type);
-    console.log('Prev State: ', store.getState());
-    console.log(`Action: `, action);
-    next(action);
-    console.log(`New State: `, store.getState());
-    console.groupEnd();
-  };
-  
