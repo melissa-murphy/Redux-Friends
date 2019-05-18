47a6c6c6a76441d9cff2798525d9f7b9419a0bda
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-18 16:30:03 -0700

    friendList, prep for routing

---
+++ friendsv2/src/components/FriendList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function FriendList(props) {
+  return (
+    <div className="smurf info">
+      {props.friends.map(friend => (
+        <div className="friend-card" key={friend.id}>
+        <h4>{friend.name}</h4>
+        <p>{friend.age}</p>
+        <p>{friend.height}</p>
+        <button onClick={() => props.removeFriends(friend.id)}>-friend</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FriendList;
